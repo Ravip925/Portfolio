f3ad70eff49cc6ddc32c8c4c7d935d0b1d26dd4b
“Ravi <patilravi400@gmail.com>
2022-12-29 20:34:18 +0530

    updated

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-function sendMail() {
-    var link = "mailto:patilravi400@gmail.com"
-    + "?cc=myCCaddress@example.com"
-    + "&subject=" + escape("This is my subject")
-    + "&body=" + escape(document.getElementById('message').value)
-;
+function sendEmail() {
+  var form = document.getElementById("form");
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (form.checkValidity()) {
+    // The form is valid, so send the email
+    var email = document.getElementById("email").value;
 
-window.location.href = link;
+    if (email.match(mailformat)) {
+      return true;
+    } else {
+      alert("You have entered an invalid email address!");
+      return false;
+    }
+    
 }
-
+}
 
 const toggle = document.getElementById("toggle");
 const sidebar = document.getElementById("sidebar");
 
-document.onclick = function(e){
-    if(e.target.id !== 'sidebar' && e.target.id !== 'toggle'){
-        toggle.classList.remove('active');
-    sidebar.classList.remove('active');
-    }
-}
+document.onclick = function (e) {
+  if (e.target.id !== "sidebar" && e.target.id !== "toggle") {
+    toggle.classList.remove("active");
+    sidebar.classList.remove("active");
+  }
+};
 
-toggle.onclick = function(){
-    toggle.classList.toggle('active');
-    sidebar.classList.toggle('active');
-}
+toggle.onclick = function () {
+  toggle.classList.toggle("active");
+  sidebar.classList.toggle("active");
+};
